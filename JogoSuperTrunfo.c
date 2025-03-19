1a08db298fbc05ed560de95fd3147b5030b2e745
Thaís Costa <202503663564@alunos.estacio.br>
2025-03-19 19:13:37 +0000

    Arquivo anterior com problemas

--- JogoSuperTrunfo.c
+++ JogoSuperTrunfo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+// Tema 1 - Cadastro das Cartas Desafio Novato
+// Tema 1 - Cadastro das Cartas Desafio Aventureiro
 
 int main() {
       
@@ -12,6 +13,11 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     //Desafio Super trunfo Novato
     //Criar varáveis
+    //Densidade Populacional: 8102.47 hab/km² (INCLUIR NO DESAFIO AVENTUREIRO)
+    //PIB per Capita: 56724.32 reais (INCLUIR NO DESAFIO AVENTUREIRO)
+    //Calcular a Densidade Populacional: Divida a população da cidade pela sua área. O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado. Armazene esse valor em uma variável do tipo float.
+    //Calcular o PIB per Capita: Divida o PIB da cidade pela sua população. O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade. Armazene esse valor em uma variável do tipo float.
+    //Exibir os Resultados: Além de exibir as informações lidas do usuário (como no nível básico), seu programa também deve exibir a densidade populacional e o PIB per capita calculados para cada cidade. Formate os valores de ponto flutuante com duas casas decimais.
 
     char estado1,estado2;
     char codigo1[20],codigo2[20];
@@ -20,6 +26,8 @@ int main() {
     float area1,area2;
     float Pib1,Pib2;
     int pontosturistico1,pontosturistico2;
+    float densidpopquociente1,densidpopquociente2;
+    float percapitaquociente1,percapitaquociente2;
 
     // CARTA 1
     //Criar impressão de mensagem para solicitação de informações
@@ -46,6 +54,11 @@ int main() {
     printf("numero de pontos turisticos :\n");
     scanf("%d",&pontosturistico1);
 
+
+  // Calculos
+    densidpopquociente1 = (float) populacao1 / area1;
+    percapitaquociente1 = (float) Pib1 / populacao1;
+
     // CARTA 2 
     //Criar impressão de mensagem para solicitação de informações
     //Criar recebimanto de valor via scanf
@@ -71,6 +84,11 @@ int main() {
     printf("numero de pontos turisticos :\n");
     scanf("%d",&pontosturistico2);
 
+    // Calculo
+    densidpopquociente2 = (float) populacao2 / area2;
+    percapitaquociente2 = (float) Pib2 / populacao2;
+
+
   //Criar impressão dos valores recebidos da Carta 1
    
     printf("Carta 1!\n");
@@ -79,10 +97,13 @@ int main() {
     printf("estado: %c\n",estado1);
     printf("Codigo:%s\n",codigo1);
     printf("Cidade:%s\n",cidade1);
-    printf("Populacao:%d\n",&populacao1);
+    printf("Populacao:%d\n",populacao1);
     printf("Area:%f km²\n",area1);
     printf("PIB:%f bilhoes de reais.\n",Pib1);
     printf("Pontos Turisticos:%d\n",pontosturistico1);
+    printf ("Densidade Populacional:%.2f hab/km²\n", densidpopquociente1);
+    printf("PIB per Capita:%.2f reais\n", percapitaquociente1);
+
 
     //Criar impressão dos valores recebidos da Carta 2 
 
@@ -92,10 +113,12 @@ int main() {
     printf("estado: %c\n",estado2);
     printf("Codigo: %s\n",codigo2);
     printf("Cidade:%s\n",cidade2);
-    printf("Populacao:%d\n",&populacao2);
+    printf("Populacao:%d\n",populacao2);
     printf("Area:%f km²\n",area2);
     printf("PIB:%f bilhoes de reais.\n",Pib2);
     printf("Pontos Turisticos:%d\n",pontosturistico2);
+    printf ("Densidade Populacional:%.2f hab/km²\n", densidpopquociente2);
+    printf("PIB per Capita:%.2f reais\n", percapitaquociente2);
 
     return 0;
 }
